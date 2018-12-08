fa01c91d0733fc82bccd39b5a3e97dc61fea646f
khemraj-dev <khemraj.47billion@gmail.com>
2018-12-08 14:11:58 +0530

    update test for generic service

--- src/app/shared/generic.service.spec.ts
+++ src/app/shared/generic.service.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { GenericService } from './generic.service';
 
 describe('GenericService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       providers: [GenericService]
     });
   });
